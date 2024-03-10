d6e7a05bd6a84feabc23499bef1503a69a8d1685
maxsarea <162867949+maxsarea@users.noreply.github.com>
2024-03-10 12:35:02 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,207 +1,97 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Lab1());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+/// this is your APP Main screen configuration
+class Lab1 extends StatelessWidget {
+  Lab1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: LAb1HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool isExpanded = true;
-  bool isCircle = true;
-  bool isVisible = true;
-  int count = 0;
+/// this is a template to start building a UI
+/// to start adding any UI you want change what comes after the [ body: ] tag below
+class LAb1HomePage extends StatelessWidget {
+  LAb1HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: const Color(0xfff4f4f4),
+      /*******************--[focus here 🧐]--*******************/
       appBar: AppBar(
-        title: Text(
-          'Lab 3',
-          style: TextStyle(fontSize: 18),
-        ),
+        leading: const Icon(Icons.stars),
+        title: const Text('Lab 1'),
+        backgroundColor: Colors.teal,
+        elevation: 4,
       ),
-      body: AnimatedContainer(
-        margin: EdgeInsets.all(20),
-        duration: Duration(milliseconds: 450),
-        width: screenWidth,
-        height: isExpanded ? screenHeight : 60,
-        // to 60 when click on it, to screen height when also click on it
-        color: Colors.indigo,
-        child: Stack(
-          children: [
-            Container(
-              height: 60,
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              color: Colors.yellow,
-              child: InkWell(
-                onTap: () {
-                  setState(() {
-                    isExpanded = !isExpanded;
-                  });
-                },
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                        child: Text(
-                      'Click to Expand',
-                      style: TextStyle(fontSize: 18),
-                    )),
-                    Icon(Icons.keyboard_arrow_up),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    changeShapes(screenWidth),
-                    flutterChanger(screenWidth),
-                    showHide(screenWidth),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+      body: myWidget(),
+      /*******************--[focus here 🧐]--*******************/
+    );
+  }
+
+  Widget myWidget() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          /*******************--[focus here 🧐]--*******************/
+          SizedBox(height: 25,),
+          exercise1(),
+          SizedBox(height: 25,),
+          exercise2(),
+          SizedBox(height: 25,),
+          exercise3(),
+          SizedBox(height: 25,),
+          exercise4(),
+          SizedBox(height: 25,),
+          exercise5(),
+          SizedBox(height: 25,),
+          exercise6(),
+          /*******************--[focus here 🧐]--*******************/
+        ],
       ),
     );
   }
 
-  Widget changeShapes(double width) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: Container(
-            margin: EdgeInsets.all(8),
-            color: Colors.white,
-            width: width,
-            height: 150,
-
-            /// ********** Do changes in child below ********** ///
-            child: InkWell(
-              onTap: () {
-                setState(() {
-                  isCircle = !isCircle;
-                });
-              },
-              child: Row(
-                children: [
-                  Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: isCircle ? Colors.blue : Colors.red,
-                      shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
-                    ),
-                  ),
-                ],
-              ),
-            )),
-      ),
+  /// TODO: Implement Exercises below as per the handed Document
+  Widget exercise1() {
+    return FlutterLogo(
+      size: 100,
     );
   }
 
-  Widget flutterChanger(double width) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: Container(
-            margin: EdgeInsets.all(8),
-            color: Colors.white,
-            width: width,
-            height: 150,
-
-            /// ********** Do changes in child below ********** ///
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FlutterLogo(
-                  style: changeFlutterLogo(),
-                  size: 100,
-                ),
-                IconButton(
-                    onPressed: () {
-                      setState(() {
-                        count++;
-                        if (count > 2) count = 0;
-                      });
-                    },
-                    icon: Icon(
-                      Icons.refresh,
-                      size: 30,
-                    ))
-              ],
-            )),
-      ),
+  Widget exercise2() {
+    return FlutterLogo(
+      size: 100,
     );
   }
 
-  FlutterLogoStyle changeFlutterLogo() {
-    if (count == 0)
-      return FlutterLogoStyle.markOnly;
-    else if (count == 1) {
-      return FlutterLogoStyle.horizontal;
-    } else
-      return FlutterLogoStyle.stacked;
+  /// TODO : print on the screen on Pressed when clicking on the button, and print on Long Pressed when long click to Button
+  Widget exercise3() {
+    return FlutterLogo(
+      size: 100,
+    );
   }
 
-  Widget showHide(double width) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: Container(
-            margin: EdgeInsets.all(8),
-            color: Colors.white,
-            width: width,
-            height: 150,
+  Widget exercise4() {
+    return FlutterLogo(
+      size: 100,
+    );
+  }
 
-            /// ********** Do changes in child below ********** ///
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Visibility(
-                  visible: isVisible,
-                  child: Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                    size: 100,
-                  ),
-                ),
-                InkWell(
-                    onTap: () {
-                      setState(() {
-                        isVisible = !isVisible;
-                      });
-                    },
-                    child: Icon(!isVisible ? Icons.lock_open : Icons.lock,
-                        color: Colors.orange, size: 100)),
-              ],
-            )),
-      ),
+  Widget exercise5() {
+    return FlutterLogo(
+      size: 100,
+    );
+  }
+  Widget exercise6() {
+    return FlutterLogo(
+      size: 100,
     );
   }
 }
